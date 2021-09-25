1181fa89fcc6037e03f48acf49bda29297fc333f
Prashanth <imprash4u@gmail.com>
2021-09-26 02:45:24 +0530

    Added: Creating Employee Payroll service to read from console and write to console

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayRollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayRollService.java
@@ -1,7 +1,30 @@
 package com.bridgelabz.employeepayroll;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class EmployeePayRollService {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<EmployeePayroll> employeePayrolls = new ArrayList<EmployeePayroll>();
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Payroll Service ");
+        EmployeePayRollService service = new EmployeePayRollService();
+        service.readData();
+        service.writeData();
+    }
+
+    private void readData() {
+        EmployeePayroll employeePayroll = new EmployeePayroll();
+        System.out.println("Enter Id");
+        employeePayroll.setId(scanner.nextInt());
+        System.out.println("Enter Name");
+        employeePayroll.setName(scanner.next());
+        System.out.println("Enter Salary");
+        employeePayroll.setSalary(scanner.nextInt());
+        employeePayrolls.add(employeePayroll);
+    }
+
+    private void writeData() {
+        System.out.println(employeePayrolls);
     }
 }
---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayroll.java
@@ -0,0 +1,51 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayroll {
+    private int id;
+    private String name;
+    private int salary;
+
+    public EmployeePayroll() {
+
+    }
+
+    public EmployeePayroll(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayroll{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
